10b236031226a68fbe43d4513f0ca5bcc836b9df
Gerard Capes <gerard.capes@manchester.ac.uk>
2021-07-14 21:23:56 +0100

    Use vectorised search for matches
    This reduced search time from 9.57s to 0.01s.

--- lookup.py
+++ lookup.py
@@ -4,10 +4,8 @@ ref = pd.read_excel('./Copy of 3207 Affected markets for a CMC Variation.xlsx', 
                    skiprows=14, header=0)
 search = pd.read_excel('./Register CMC-05-Affected documents 02.07.2021.xlsx', sheet_name="All docs",
                        header=0)
-results = pd.DataFrame()
-for id in ref['Registration ID']:
-    matches = search.loc[(search['Registration ID'] == id) & (search["Title"].str.contains("P7-00D"))]
-    if not matches.empty:
-        results = results.append(matches)
 
-results.to_excel("matches.xlsx")
+matches = search.loc[(search['Registration ID'].isin(ref['Registration ID']))
+                     & (search["Title"].str.contains("P7-00D"))]
+
+matches.to_excel("matches.xlsx")
