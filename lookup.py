8f9088d231c8e4f196995e6c6c3a1be9cffa320f
Gerard Capes <gerard.capes@manchester.ac.uk>
2021-08-02 20:40:26 +0100

    Specify document type as string variable

--- lookup.py
+++ lookup.py
@@ -4,8 +4,8 @@ ref = pd.read_excel('./Copy of 3207 Affected markets for a CMC Variation.xlsx', 
                    skiprows=14, header=0)
 search = pd.read_excel('./Register CMC-05-Affected documents 02.07.2021.xlsx', sheet_name="All docs",
                        header=0)
-
+doc_type = "P3-03"
 matches = search.loc[(search['Registration ID'].isin(ref['Registration ID']))
-                     & (search["Title"].str.contains("P7-00D"))]
+                     & (search["Title"].str.contains(doc_type))]
 
 matches.to_excel("matches.xlsx")
