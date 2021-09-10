92e6cd5224bf2df2e5c3ba738adf0ccaa814a7a0
Gerard Capes <gerard.capes@manchester.ac.uk>
2021-09-10 14:58:30 +0100

    Return matches as csv in existing column

--- lookup.py
+++ lookup.py
@@ -4,8 +4,18 @@ ref = pd.read_excel('./Copy of 3207 Affected markets for a CMC Variation.xlsx', 
                    skiprows=14, header=0)
 search = pd.read_excel('./Register CMC-05-Affected documents 02.07.2021.xlsx', sheet_name="All docs",
                        header=0)
-doc_type = "P3-03"
-matches = search.loc[(search['Registration ID'].isin(ref['Registration ID']))
-                     & (search["Title"].str.contains(doc_type))]
+doc_type = "P3-05"
 
-matches.to_excel(doc_type + "_matches.xlsx")
+match_col = pd.Series(index=ref.index, dtype=str)
+for index, ref_id in enumerate(ref['Registration ID']):
+    matches = search.loc[(search['Registration ID'] == ref_id) & (search["Title"].str.contains(doc_type))]
+    if not matches.empty:
+        ref_nums = matches['Reference Number'].to_string(index=False).replace("\n", ", ")
+    else:
+        ref_nums = ""
+    match_col[index] = ref_nums
+ref[doc_type] = match_col
+
+
+
+ref.to_excel("matches.xlsx")
